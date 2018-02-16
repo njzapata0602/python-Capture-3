1e933f02f3dae8c47b2574cfd4e446f6017b8844
njzapata0602 <33432035+njzapata0602@users.noreply.github.com>
2018-02-16 12:59:46 -0700

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,8 @@
+#Nick Zapata - ch 3 - ex 1 - 2/15/18
+
+Hour = int(input('Enter Hours: '))
+Rate = int(input('Enter Rate: '))
+
+pay = Hour * Rate
+
+print(pay)
---
+++ Exercise 2.py
@@ -0,0 +1,9 @@
+#Nick Zapata - ch 3 - ex 2 - 2/15/18
+
+print('Please enter a numeric input')
+Hour = int(input('Enter Hours: '))
+Rate = int(input('Enter Rate: '))
+
+pay = Hour * Rate
+
+print(pay)
